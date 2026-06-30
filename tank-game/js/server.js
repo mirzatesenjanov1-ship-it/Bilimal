9044e0b7b01303bfc942a5c48899dd36be6d7095
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-06-30 14:42:30 +0600

    Add server functionality for multiplayer tank game
    Implemented server-side logic for player connection, movement, and chat in a multiplayer tank game.

--- tank-game/js/server.js
+++ tank-game/js/server.js
@@ -1,1 +1,44 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Сиздин статикалык файлдарды (HTML, JS, CSS) берүү
+app.use(express.static('tank-game'));
+
+let players = {};
+
+io.on('connection', (socket) => {
+    console.log('Жаңы оюнчу кошулду: ' + socket.id);
+
+    // Жаңы оюнчуга баштапкы координаттарды берүү
+    players[socket.id] = { x: 380, y: 500, hp: 3 };
+    
+    // Башка оюнчуларга жаңы оюнчу жөнүндө кабарлоо
+    io.emit('currentPlayers', players);
+
+    // Оюнчу кыймылдаганда
+    socket.on('playerMovement', (movementData) => {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+
+    // Кландык чат үчүн
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Оюнчу чыгып кетти: ' + socket.id);
+        delete players[socket.id];
+        io.emit('currentPlayers', players);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Сервер 3000-портто иштеп жатат. Мультиплеер активдүү.');
+});
