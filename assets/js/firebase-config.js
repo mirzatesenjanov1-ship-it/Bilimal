6780d2794790cbb497a97094b85df439ff9d7313
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-07-10 02:40:35 +0600

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,51 +1,73 @@
-// Firebase Web App Modules Production SDK Initialization Framework
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { 
-    getAuth, 
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { 
-    getDatabase, 
-    ref, 
-    set, 
-    push, 
-    update, 
-    get, 
-    onValue, 
-    query, 
-    orderByChild, 
-    equalTo 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-import { 
-    getStorage, 
-    ref as storageRef, 
-    uploadBytes, 
-    getDownloadURL 
-} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
+// Firebase v10+ CDN Modular SDK Imports
+import { initializeApp, getApp, getApps } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getDatabase, ref, set, get, update, push, remove, onValue, query, orderByChild, equalTo, limitToLast, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
-// Production Configuration Parameters Matrix
 const firebaseConfig = {
-    apiKey: "AIzaSyAsRjj_5VoQwZA7hSBWhkQ58UvUnct-b28",
-    authDomain: "bilimal-org.firebaseapp.com",
-    databaseURL: "https://bilimal-org-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bilimal-org",
-    storageBucket: "bilimal-org.firebasestorage.app",
-    messagingSenderId: "241750360816",
-    appId: "1:241750360816:web:a991434eb5afbc470d7835",
-    measurementId: "G-9GSQV60QV0"
+  apiKey: "AIzaSyAsRjj_5VoQwZA7hSBWhkQ58UvUnct-b28",
+  authDomain: "bilimal-org.firebaseapp.com",
+  databaseURL: "https://bilimal-org-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bilimal-org",
+  storageBucket: "bilimal-org.firebasestorage.app",
+  messagingSenderId: "241750360816",
+  appId: "1:241750360816:web:a991434eb5afbc470d7835",
+  measurementId: "G-9GSQV60QV0"
 };
 
-// Instance Instantiation Engine
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-const storage = getStorage(app);
+let app;
+let auth = null;
+let db = null;
+let analytics = null;
+let firebaseReady = false;
+let firebaseError = null;
+let isFirebaseAvailable = false;
 
-export { 
-    app, auth, db, storage,
-    onAuthStateChanged, signInWithEmailAndPassword, signOut,
-    ref, set, push, update, get, onValue, query, orderByChild, equalTo,
-    storageRef, uploadBytes, getDownloadURL
+try {
+  if (!getApps().length) {
+    app = initializeApp(firebaseConfig);
+  } else {
+    app = getApp();
+  }
+  
+  auth = getAuth(app);
+  db = getDatabase(app);
+  firebaseReady = true;
+  isFirebaseAvailable = true;
+
+  try {
+    analytics = getAnalytics(app);
+  } catch (e) {
+    console.warn("Firebase Analytics could not be initialized:", e);
+  }
+} catch (error) {
+  firebaseError = error.message;
+  isFirebaseAvailable = false;
+  console.error("Firebase Initialization Error:", error);
+}
+
+export {
+  app,
+  auth,
+  db,
+  analytics,
+  firebaseReady,
+  firebaseError,
+  isFirebaseAvailable,
+  serverTimestamp,
+  ref,
+  set,
+  get,
+  update,
+  push,
+  remove,
+  onValue,
+  query,
+  orderByChild,
+  equalTo,
+  limitToLast,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  logEvent
 };
