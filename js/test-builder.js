3faa5d08ab455f90a8e597942817bafde6e052e5
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-08-08 23:59:26 +0600

    Update test-builder.js

--- js/test-builder.js
+++ js/test-builder.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('editBadge').style.display = 'inline-block';
                 await loadExistingTest(editTestId);
             } else {
-                addQuestion('single'); // Жаңы тест түзүүдө 1 дефолт суроо кошуу
+                addQuestion('single'); // Жаңы тест түзүүдө 1 дефолт суроо кошу
             }
         } else {
             alert("Тест түзүү же оңдоо үчүн системага киришиңиз керек!");
